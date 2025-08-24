2809911a804602cd059c999de925c2dd2703135b
Dhruvsingh121 <GodZeusOP@gmail.com>
2025-08-24 11:09:27 +0530

    Added Client as normal folder

---
+++ Client/src/components/Post/Post.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Comment from "../../img/comment.png";
+import Share from "../../img/share.png";
+import Heart from "../../img/like.png";
+import NotLike from "../../img/notlike.png";
+
+import "./Post.css";
+import { useSelector } from "react-redux";
+import { likePost } from "../../api/PostRequest";
+const Post = ({ data }) => {
+  const { user } = useSelector((state) => state.authReducer.authData);
+
+  const [liked, setLiked] = useState(data.likes.includes(user._id));
+  const [likes, setLikes] = useState(data.likes.length);
+
+  const handleLike = () => {
+    setLiked((prev) => !prev);
+    likePost(data._id, user._id);
+    liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
+  };
+  // console.log(data);
+  return (
+    <div className="Post">
+      <img
+        src={data.image ? process.env.REACT_APP_PUBLIC_FOLDER + data.image : ""}
+        alt=""
+      />
+
+      <div className="postReact">
+        <img
+          src={liked ? Heart : NotLike}
+          alt=""
+          style={{ cursor: "pointer" }}
+          onClick={handleLike}
+        />
+        <img src={Comment} alt="" />
+      </div>
+
+      <span style={{ color: "var(--gray)", fontSize: "12px" }}>
+        {likes} Likes
+      </span>
+
+      <div className="detail">
+        <span>
+          <b>{data.name}</b>
+        </span>
+        &nbsp;
+        <span>{data.desc}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
